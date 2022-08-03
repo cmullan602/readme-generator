864aeaf4bcd896b22dc4a11b57b5e7b77d8505db
Connor Mullan <cmullan602@gmail.com>
2022-08-02 22:14:22 -0700

    worked on renderLicenseBadge.

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,6 +1,21 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+
+  if(license === 'MIT License'){
+    return' ![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)'
+  } else if (license === 'Mozilla Public License 2.0'){
+    return ' ![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)'
+  }else if (license === 'Eclipse Public License'){
+    return ' ![License](https://img.shields.io/badge/License-EPL_1.0-red.svg)'
+  }else if(license === 'The Perl License'){
+    return ' ![License: Artistic-2.0](https://img.shields.io/badge/License-Perl-0298c3.svg)'
+  }else if(license === 'Open Database License'){
+    return ' ![License: ODbL](https://img.shields.io/badge/License-ODbL-brightgreen.svg)'
+  }else{
+    return ' '
+  }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
