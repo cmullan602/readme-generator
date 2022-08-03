7e8e7669af93f34950bee1a01b58cab138ce9ae9
Connor Mullan <cmullan602@gmail.com>
2022-08-02 22:11:56 -0700

    added questions array to index.

--- Develop/index.js
+++ Develop/index.js
@@ -5,7 +5,56 @@ const generateMarkdown = require("./utils/generateMarkdown");
 const path = require('path');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        name: "title",
+        type: "input",
+        message: "What is the title of your project?",
+    },
+    {
+        name: "description",
+        type: "input",
+        message: "Please describe your project.",
+    },{
+        name: "installation",
+        type: "input",
+        message: "How do you install your project?",
+    }, {
+        name: "usage",
+        type: "input",
+        message: "How do you use your project?",
+    }, {
+        type: 'list',
+        name: 'license',
+        message: 'Choose a License',
+        choices: [
+         'MIT License',
+           'Mozilla Public License 2.0',
+           'Eclipse Public License',
+           'The Perl License',
+           'Open Database License',
+           'None'
+        ],
+        initial: 1
+      }, {
+        name: "contributing",
+        type: "input",
+        message: "How can others contribute to your project?",
+    }, {
+        name: "tests",
+        type: "input",
+        message: "Please provide examples on how to test your project.",
+    }, {
+        name: "github",
+        type: "input",
+        message: "What is your github username?",
+    },
+    {
+        name: "email",
+        type: "input",
+        message: "What is your email?",
+    },
+]
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
