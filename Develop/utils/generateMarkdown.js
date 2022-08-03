e969ba7fa12856a829e12d5d6e17ea5b7cc8cd80
Connor Mullan <cmullan602@gmail.com>
2022-08-02 22:16:34 -0700

    worked on creatMarkdown function.

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -39,9 +39,49 @@ function renderLicenseSection(license) {
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return`# ${data.title}   
+  ${renderLicenseBadge(data.license)}
+## Description
+
+${data.description}
+
+## Table of Contents 
+
+* [Installation](#installation)
+* [Usage](#usage)
+${renderLicenseLink(data.license)}
+* [Contributing](#contributing)
+* [Tests](#tests)
+* [Questions](#questions)
+
+## Installation
+
+${data.installation}
+
+## Usage
+
+${data.usage}
+
+${renderLicenseSection(data.license)}
+
+## Contributing
+
+${data.contributing}
+
+## Tests
+
+${data.tests}
+
+## Questions
+
+If you have any questions please contact me through the methods below:
+
+  Github: [${data.github}](https://github.com/${data.github})
+
+  Email: [${data.email}](mailto:${data.email})
 
 `;
+
 }
 
 module.exports = generateMarkdown;
